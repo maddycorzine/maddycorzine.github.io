c9945d1126b869b4be0f9e6e55e5c707f0e2f3c4
maddycorzine <maco2972@colorado.edu>
2023-11-07 10:05:42 -0700

    ica 11 push

---
+++ ica/ica11.js
@@ -0,0 +1,16 @@
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+
+function result() {
+  let newStory = storyText;
+  }
+
+  if (document.getElementById.checked) {
+    const height = `${Math.round(300*0.0714286)} stone`;
+    const color =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+  }
+}
